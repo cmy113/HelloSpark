5408b13277f9845d4bfeca0aa16afe0135bb0532
cmy113 <chenming.yong13@gmail.com>
2021-05-30 11:31:04 +0800

    create your own logger and adding it

--- HelloSpark.py
+++ HelloSpark.py
@@ -1,4 +1,15 @@
 from pyspark.sql import *
-
+from lib.logger import Log4J
 if __name__ =="__main__":
     print("Starting Hello Spark.")
+    spark = SparkSession.builder \
+        .appName("Hello Spark") \
+        .master("local[3]") \
+        .getOrCreate()
+
+    logger = Log4J(spark)
+    logger.info("Starting HelloSpark")
+
+    logger.info("Finished HelloSpark")
+
+    spark.stop()
