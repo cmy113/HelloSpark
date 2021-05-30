1d877a99719aab729b31828257a95058e363c271
cmy113 <chenming.yong13@gmail.com>
2021-05-30 14:04:00 +0800

    1. Do some wide transformation to dataframe 2. Pause the program to check on the spark ui

--- HelloSpark.py
+++ HelloSpark.py
@@ -2,7 +2,7 @@ import sys
 
 from pyspark.sql import *
 from lib.logger import Log4J
-from lib.utils import get_spark_app_config, load_survey_df
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 if __name__ =="__main__":
     conf = get_spark_app_config()
@@ -23,7 +23,14 @@ if __name__ =="__main__":
         sys.exit(-1)
 
     survey_df = load_survey_df(spark,sys.argv[1])
-    survey_df.show()
+
+    # Force it to repartition into two since local machine only have 1 partition
+    partitioned_survey_df = survey_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+    logger.info(count_df.collect())
+
+    # Pause the program so that we can see the UI in localhost:4040
+    input("Press Enter")
 
     logger.info("Finished HelloSpark")
 
