0c8f60f8cb579c62d497dda217ffcda243cb8afe
cmy113 <chenming.yong13@gmail.com>
2021-05-30 12:39:23 +0800

    use spark.conf to define spark configuration without hardcoding inside the application, use a config file instead

--- HelloSpark.py
+++ HelloSpark.py
@@ -1,14 +1,21 @@
 from pyspark.sql import *
 from lib.logger import Log4J
+from lib.utils import get_spark_app_config
+
 if __name__ =="__main__":
+    conf = get_spark_app_config()
     print("Starting Hello Spark.")
     spark = SparkSession.builder \
-        .appName("Hello Spark") \
-        .master("local[3]") \
+        .config(conf=conf) \
         .getOrCreate()
 
     logger = Log4J(spark)
     logger.info("Starting HelloSpark")
+
+    conf_out = spark.sparkContext.getConf()
+
+    # Print out all the configuration
+    logger.info(conf_out.toDebugString())
 
     logger.info("Finished HelloSpark")
 
